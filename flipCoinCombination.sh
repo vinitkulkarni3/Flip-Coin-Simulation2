61ce04eab59cfea4c906104b7276bd3559b82efa
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-21 01:17:22 +0530

    Added:Head or Tail generating

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #head or tail count and inserting it into dictionary
 
+#1 generating head or tail
+ranNum=$(($RANDOM%2+1))
+if [ $ranNum == 1 ]
+then
+	echo "Head"
+else
+        echo "Tail"
+fi
