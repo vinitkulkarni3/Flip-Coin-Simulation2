31f8789b813a67a4b859a7ad6d44252bda7f1d11
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-21 01:34:28 +0530

    Added:Head Tail sorting and winnig combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,6 +3,8 @@
 
 declare -A arrsingle
 declare -A arrdouble
+declare -A arrsort1
+declare -A arrsort2
 #2 percentage of head and tail
 ranNum=$(($RANDOM%2+1))
 for (( i=0; i<5; i++ ))
@@ -43,6 +45,13 @@ echo "Head count  "$count1
 echo "Tail count  "$count2
 echo ""
 
+#largest count
+if [ $count1 -gt $count2 ]
+then
+        echo "Head count won "$count1
+else
+        echo "Tail count won "$count2
+fi
 
 echo ""
 answ1=`echo - | awk '{print '$count1' / '$range'}'`
@@ -51,6 +60,22 @@ answ2=`echo - | awk '{print '$count2' / '$range'}'`
 echo "Head% "$answ1
 echo "Tail% "$answ2
 echo ""
+
+#inserting into array
+arrsort1[H]=$answ1
+arrsort1[T]=$answ2
+echo "Single sorted arrya elemts are...->"
+for (( i=0; i<5; i++ ))
+do
+        for (( j=$i; j<5; j++ ))
+        do
+                temp=${arrsort1[$i]}
+                arrsort1[$i]=${arrsort1[$j]}
+                arrsort1[$j]=$temp
+        done
+done
+echo ${arrsort1[@]}
+echo "-------------------------------------------------------------------"
 
 
 
@@ -106,7 +131,23 @@ echo "TH count "$THcount
 echo "TT count "$TTcount
 
 
+#finding large count
+if [ $HHcount -gt $HTcount ] && [ $HHcount -gt $THcount ] && [ $HHcount -gt $TTcount ]
+then
+echo "HH count won "$HHcount
+elif [ $HTcount -gt $THcount ] && [ $HTcount -gt $TTcount ]
+then
+echo "HT count won "$HTcount
+elif [ $THcount -gt $TTcount ]
+then
+echo "TH count won "$THcount
+else
+echo "TT count won "$TTcount
+fi
+
+
 range1=${#arrdouble[@]}
+
 answer1=`echo - | awk '{print '$HHcount' / '$range1'}'`
 answer2=`echo - | awk '{print '$HTcount' / '$range1'}'`
 answer3=`echo - | awk '{print '$THcount' / '$range1'}'`
@@ -118,3 +159,22 @@ echo "HT% "$answer2
 echo "TH% "$answer3
 echo "TT% "$answer4
 
+#inserting into array
+arrsort2[HH%]=$answer1
+arrsort2[HT%]=$answer2
+arrsort2[TH%]=$answer3
+arrsort2[TT%]=$answer4
+
+temp=0
+echo ""
+echo "Doublet sorted array elements are..."
+for (( i=0; i<5; i++ ))
+do
+        for (( j=$i; j<5; j++ ))
+        do
+                temp=${arrsort2[$i]}
+                arrsort2[$i]=${arrsort2[$j]}
+                arrsort2[$j]=$temp
+        done
+done
+echo ${arrsort2[@]}
